6c797734ae24d476c4b2aad862b523ca9c3d9f63
rainmaker490 <varun@verve3.io>
2016-02-22 10:14:10 -0800

    Update Install Script

--- install/install.sh
+++ install/install.sh
@@ -4,12 +4,16 @@ whereAmI="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $whereAmI
 
 DEPENDENCIES_FILE_PATH=$(echo $(pwd)/dependencies.txt)
-# echo $DEPENDENCIES_FILE_PATH
+DATABASE_DIRECTORY_PATH=$whereAmI/../data/
+DATABASE_FILE_NAME=database.sqlite
 
+# create environment :
 cd ../src
 conda create -n march-madness-kaggle python
 source activate march-madness-kaggle
 while IFS='' read -r dependency || [[ -n "$dependency" ]] ; do
-	# echo hi
 	conda install  $dependency
 done < $DEPENDENCIES_FILE_PATH
+cd $whereAmI
+
+# database import :
